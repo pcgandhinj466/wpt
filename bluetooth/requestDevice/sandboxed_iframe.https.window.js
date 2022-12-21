8088864071d338e31783424de3f6ecc3f16d3851
gsinafirooz <sinafirooz@google.com>
2022-12-20 18:13:07 -0800

    Ignore opaque origins request to connect to Bluetooth devices
    Bug: 1375133
    Change-Id: Idf737c1806eac4342e0fe716e2561e51aa127f53

---
+++ bluetooth/requestDevice/sandboxed_iframe.https.window.js
@@ -0,0 +1,23 @@
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=/bluetooth/resources/bluetooth-test.js
+// META: script=/bluetooth/resources/bluetooth-fake-devices.js
+
+'use strict';
+
+bluetooth_test(async (t) => {
+  const iframeWatcher = new EventWatcher(t, iframe, ['load']);
+  let iframe = document.createElement('iframe');
+  iframe.src = '';
+  iframe.setAttribute('sandbox')
+  iframe.allow = 'bluetooth';
+  document.body.appendChild(iframe);
+  await iframeWatcher.wait_for('load');
+
+  try {
+    navigator.bluetooth.requestDevice();
+  } catch (e) {
+    assert_unreached(
+        'requestDevice() should not fail when called from a sandboxed iframe inside a document with no opaque origin.');
+  }
+}, 'Calls to Bluetooth APIs from an origin with opaque top origin get blocked.');
